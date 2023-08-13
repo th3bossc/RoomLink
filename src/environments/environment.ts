6ba31911953050b35843a7ef712493c778926c52
Th3BossC <diljith2003@gmail.com>
2023-08-13 10:31:53 +0530

    changed backend url to hosted one

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production : false,
-    // socketUrl : 'https://chatappbackend-selm.onrender.com',
-    socketUrl : 'http://localhost:5000',
-    // apiUrl : 'https://chatappbackend-selm.onrender.com/',
-    apiUrl : 'http://localhost:5000/',
+    socketUrl : 'https://chatappbackend-selm.onrender.com',
+    // socketUrl : 'http://localhost:5000',
+    apiUrl : 'https://chatappbackend-selm.onrender.com/',
+    // apiUrl : 'http://localhost:5000/',
 }
